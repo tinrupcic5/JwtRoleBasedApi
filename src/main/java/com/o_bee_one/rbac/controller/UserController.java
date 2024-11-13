942dd928922ba7693f3dba2c9ed0971f05d9bee1
tinrupcic5 <tinrupcic5@gmail.com>
2024-11-13 09:27:47 +0100

    Remove wildcard

--- src/main/java/com/o_bee_one/rbac/controller/UserController.java
+++ src/main/java/com/o_bee_one/rbac/controller/UserController.java
@@ -14,9 +14,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
